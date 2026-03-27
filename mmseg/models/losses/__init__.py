9725140f64125ea421e10dbada5ea25dce6853ef
ROYZHANG295 <jwang459@ford.com>
2026-03-27 08:41:52 -0400

    laplacian loss dynamic3 batchsize=12

--- mmseg/models/losses/__init__.py
+++ mmseg/models/losses/__init__.py
@@ -12,11 +12,12 @@ from .silog_loss import SiLogLoss
 from .tversky_loss import TverskyLoss
 from .utils import reduce_loss, weight_reduce_loss, weighted_loss
 from .laplacian_edge_loss import LaplacianEdgeLoss
+from .edge_aware_loss import EdgeAwareOhemCrossEntropy
 
 __all__ = [
     'accuracy', 'Accuracy', 'cross_entropy', 'binary_cross_entropy',
     'mask_cross_entropy', 'CrossEntropyLoss', 'reduce_loss',
     'weight_reduce_loss', 'weighted_loss', 'LovaszLoss', 'DiceLoss',
     'FocalLoss', 'TverskyLoss', 'OhemCrossEntropy', 'BoundaryLoss',
-    'HuasdorffDisstanceLoss', 'SiLogLoss', 'LaplacianEdgeLoss'
+    'HuasdorffDisstanceLoss', 'SiLogLoss', 'LaplacianEdgeLoss', 'EdgeAwareOhemCrossEntropy'
 ]
