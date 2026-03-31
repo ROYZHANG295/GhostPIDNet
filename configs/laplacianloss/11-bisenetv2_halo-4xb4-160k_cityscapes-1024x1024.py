0bab0356b4dd71db1e58282535a0b8421168ccd8
ROYZHANG295 <jwang459@ford.com>
2026-03-31 10:38:06 -0400

    train 160k configuration savebest

--- configs/laplacianloss/11-bisenetv2_halo-4xb4-160k_cityscapes-1024x1024.py
+++ configs/laplacianloss/11-bisenetv2_halo-4xb4-160k_cityscapes-1024x1024.py
@@ -55,3 +55,16 @@ optim_wrapper = dict(type='OptimWrapper', optimizer=optimizer)
 train_dataloader = dict(batch_size=4, num_workers=4)
 val_dataloader = dict(batch_size=1, num_workers=4)
 test_dataloader = val_dataloader
+
+train_cfg = dict(
+    type='IterBasedTrainLoop', max_iters=160000, val_interval=1000)
+val_cfg = dict(type='ValLoop')
+test_cfg = dict(type='TestLoop')
+default_hooks = dict(
+    timer=dict(type='IterTimerHook'),
+    logger=dict(type='LoggerHook', interval=50, log_metric_by_epoch=False),
+    param_scheduler=dict(type='ParamSchedulerHook'),
+    checkpoint=dict(
+        type='CheckpointHook', by_epoch=False, save_best='mIoU', interval=1000),
+    sampler_seed=dict(type='DistSamplerSeedHook'),
+    visualization=dict(type='SegVisualizationHook'))
